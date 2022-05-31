19483b8b8a7dbba3c1920da854863f6af75ca987
ErmiasBahru <ermiasbahru878@gmail.com>
2022-05-31 09:59:33 +0300

    initial

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
+print('Last digit of {} is {} and is'.format(number, last_digit), end=' ')
+if last_digit > 5:
+    print('greater than 5')
+elif last_digit == 0:
+    print('0')
+else:
+    print('less than 6 and not 0')
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+	if i != ord('q') and i != ord('e'):
+		print('{:c}'.format(i), end='')
