4276dadc286143fc142389e47eb0e85b087b97d4
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-02 21:08:55 +0300

    advance task 1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    if len(argv) - 1 != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if argv[2] not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    if argv[2] == '+':
+        print('{} + {} = {}'.format(a, b, add(a, b)))
+    elif argv[2] == '-':
+        print('{} - {} = {}'.format(a, b, sub(a, b)))
+    elif argv[2] == '*':
+        print('{} * {} = {}'.format(a, b, mul(a, b)))
+    elif argv[2] == '/':
+        print('{} / {} = {}'.format(a, b, div(a, b)))
