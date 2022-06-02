314608f0e57bd9516651c0ddbf2941f8023c6d5b
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-02 12:37:55 +0300

    fix indentation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    a, b = 10, 5
+    a = 10
+    b = 5
     print('{} + {} = {}'.format(a, b, add(a, b)))
     print('{} - {} = {}'.format(a, b, min(a, b)))
     print('{} * {} = {}'.format(a, b, mul(a, b)))
