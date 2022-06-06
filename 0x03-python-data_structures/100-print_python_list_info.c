d0cddc2ff38d290f584b8c8f85e051e83dd498d7
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-06 21:09:06 +0300

    initial commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include "Python.h"
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *list;
+	Py_ssize_t size, i;
+	PyObject *object;
+	struct _typeobject *type;
+
+	if (strcmp(p->ob_type->tp_name, "list") == 0)
+	{
+		list = (PyListObject *)p;
+		size = list->ob_base.ob_size;
+		printf("[*] Size of the Python List = %ld\n", size);
+		printf("[*] Allocated = %ld\n", list->allocated);
+		for (i = 0; i < size; i++)
+		{
+			object = list->ob_item[i];
+			type = object->ob_type;
+			printf("Element %ld: %s\n", i, type->tp_name);
+		}
+	}
+}
