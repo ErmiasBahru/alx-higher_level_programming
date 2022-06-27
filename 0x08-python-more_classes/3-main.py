c09d6e926f4531ed75ebd07070bf75330394efc8
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-27 17:17:28 +0300

    4th task

---
+++ 0x08-python-more_classes/3-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+Rectangle = __import__('3-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
