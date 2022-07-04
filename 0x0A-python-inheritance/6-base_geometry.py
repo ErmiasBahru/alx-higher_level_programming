fa85a48d229ca57aca464f4eda443f9657e14bbb
ErmiasBahru <ermiasbahru878@gmail.com>
2022-07-04 09:56:58 +0300

    6. improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains a `BaseGeometry` class"""
+
+
+class BaseGeometry():
+    pass
+
+    def area(self):
+        """Calculate the area of the geometry"""
+        raise Exception("area() is not implemented")
