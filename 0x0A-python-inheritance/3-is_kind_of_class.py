c6737dd69de00b18c5fa2414c6111badd2074603
ErmiasBahru <ermiasbahru878@gmail.com>
2022-07-04 09:25:28 +0300

    3. same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains of a single function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        Check if the object is an instance of a class
+        inherited from
+    """
+    return isinstance(obj, a_class)
