2574024b79f0b5eaa859cee4b048cbbb252c9e90
ErmiasBahru <ermiasbahru878@gmail.com>
2022-07-05 09:33:47 +0300

    initial commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class definationo of student"""
+
+
+class Student:
+    """defines a student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes instance data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            Retrieves the dictionary representation of  student
+        """
+        dictionary = self.__dict__
+        if attrs is None:
+            return dictionary
+
+        d = {k: v for k, v in dictionary.items() if k in attrs}
+        return d
+
+    def reload_from_json(self, json):
+        """
+            replaces all attributes of the Student instance
+        """
+        self.__dict__.update(json)
