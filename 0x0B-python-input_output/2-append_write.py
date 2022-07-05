eaaff8f9cec03019be5a06eea60d6273a552d976
ErmiasBahru <ermiasbahru878@gmail.com>
2022-07-05 03:39:44 +0300

    initial commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Contain a function that appends to file"""
 
+
 def append_write(filename="", text=""):
     """Handles content appendint to file"""
     with open(filename, mode="a", encoding="utf-8") as f:
