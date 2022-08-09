e478d76c9c8bee2b32f1f14490cf4cc4d09449f2
Ermias Bahru <ermiasbahru878@gmail.com>
2022-08-09 13:02:01 -0400

    temperaturs #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,4 @@
+-- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
