6f91034f4ba62e36a3afa7feda5b7fec83d092ef
Ermias Bahru <ermiasbahru878@gmail.com>
2022-08-10 05:47:58 -0400

    6. states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    `id` INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
