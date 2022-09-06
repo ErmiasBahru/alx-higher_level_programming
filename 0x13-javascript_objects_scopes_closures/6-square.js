058883a6e8222a567be35a3c1051acd57d4a3932
Ermias Bahru <ermiasbahru878@gmail.com>
2022-09-06 05:47:33 -0400

    6. square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Square1 = require('./5-square.js');
+
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
