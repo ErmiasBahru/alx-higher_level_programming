24b0e57201ad54837d8741ab8a74ae7cd65f4f27
Ermias Bahru <ermiasbahru878@gmail.com>
2022-09-23 08:04:51 -0400

    what's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
+
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf content: {}".format(html.decode('utf-8')))
