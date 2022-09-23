7bc05d5d01955ab2fe717df2466601153ff0e54f
Ermias Bahru <ermiasbahru878@gmail.com>
2022-09-23 08:06:49 -0400

    fix typo error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf content: {}".format(html.decode('utf-8')))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
