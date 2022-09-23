71335e1863cac50b101c25ec22a657b3437e0192
Ermias Bahru <ermiasbahru878@gmail.com>
2022-09-23 08:45:26 -0400

    6. post an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address, sends
+a POST  request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    values = {
+        'email': argv[2]
+    }
+    resp = requests.post(argv[1], data=values)
+    print(resp.text)
