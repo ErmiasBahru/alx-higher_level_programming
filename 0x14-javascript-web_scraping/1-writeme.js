ffecf6801059367f6f268242a8f6f5f721cda2d9
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-18 11:39:24 -0400

    1. Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * Script that writes a string to a file
+ */
+
+const process = require('process')
+const fs = require('fs')
+
+const filePath = process.argv[2]
+const content = process.argv[3]
+
+fs.writeFile(filePath, content, 'utf-8', err => {
+    if (err) {
+        console.log(err)
+    }
+})
