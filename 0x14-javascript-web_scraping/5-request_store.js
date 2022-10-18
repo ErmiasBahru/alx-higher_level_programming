333bac0584c3479516eeda08d2a93a175fba9afe
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-18 12:19:18 -0400

    5. Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/**
+ * Script that gets the contents of
+ * a webpage and stores it in a file
+ */
+const request = require('request')
+const process = require('process')
+const fs = require('fs')
+
+const url = process.argv[2]
+const filePath = process.argv[3]
+
+request.get(url, (err, _resp, body) => {
+    if (err === null) {
+        fs.writeFile(filePath, body, 'utf-8', fileError => {
+            if (fileError !== null) {
+                console.log(fileError)
+            }
+        })
+    } else {
+        console.log(err)
+    }
+})
