69a233479b3ccc93e0d5cdbd33a7620f03043408
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-18 12:14:59 -0400

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/**
+ * Script that prints the number of movies
+ * where the character “Wedge Antilles” is present.
+ */
+const request = require('request')
+const process = require('process')
+
+const url = process.argv[2]
+
+request.get(url, (err, _resp, body) => {
+    if (err === null) {
+        const data = JSON.parse(body)
+        let films = data.results
+        films = films.filter(
+            film => film.characters.find(
+                character => character.match(/18/)
+            )
+        )
+        console.log(films.length)
+    } else {
+        console.log(err)
+    }
+})
