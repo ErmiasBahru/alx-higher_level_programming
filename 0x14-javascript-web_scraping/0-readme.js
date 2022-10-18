49edbaaf1f9e1cf45c7c140fb23524b038598d3b
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-18 11:37:32 -0400

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * Script that reads from a file
+ */
+const process = require('process')
+const fs = require('fs')
+
+const filePath = process.argv[2]
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+})
