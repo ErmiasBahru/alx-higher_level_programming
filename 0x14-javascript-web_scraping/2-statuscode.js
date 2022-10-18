dc8bfa56f4824bde5bbb2a84c5ef3e1524efff38
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-18 11:51:44 -0400

    2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * Script that reads from a file
+ */
+
+const request = require('request')
+const process = require('process')
+
+const url = process.argv[2]
+
+request.get(url, (err, resp) => {
+    if (err === null) {
+        console.log(`code: ${resp.statusCode}`)
+    } else {
+        console.log(err)
+    }
+})
