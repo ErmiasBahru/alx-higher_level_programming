d6cd1a8b8914aa3375f81b93e6375a79d290c82a
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-18 12:38:50 -0400

    7. Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/**
+ * Script that prints all characters of a Star Wars movie
+ */
+
+const request = require('request');
+const process = require('process');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request.get(url, (err, response, body) => {
+    if (err === null) {
+        const film = JSON.parse(body);
+        const characters = film.characters;
+        characters.forEach((character) => {
+            request.get(character, (charError, response, body) => {
+                if (err === null) {
+                    const characterInfo = JSON.parse(body);
+                    console.log(characterInfo.name);
+                } else {
+                    console.log(charError);
+                }
+            });
+        });
+    } else {
+        console.log(err);
+    }
+});
