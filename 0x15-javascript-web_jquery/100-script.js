3b2310d990b20f861792876d70c56149793e526e
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-26 14:31:59 -0400

    10. No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,10 @@
+/**
+ * Script that updates the text color of the <header>
+ * element to red (#FF0000)
+ *
+ * script imported from the <head> tag
+ */
+window.onload = () => {
+    const header = document.querySelector('header')
+    header.style.color = '#FF0000'
+}
